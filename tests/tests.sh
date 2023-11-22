fed7e0600b052de563ff7ce591b5ea01b92ebb6a
Dorian <times.coding@gmail.com>
2023-11-22 22:58:14 +0100

    improved tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,20 +1,47 @@
+echo -e "\e[93mRunning tests...\e[0m"
+
 cd ..
 
-make clean > /dev/null 2>&1 || { echo "Error: Unable to clean the project."; exit 1; }
-make tests > /dev/null 2>&1 || { echo "Error: Unable to build and run tests."; exit 1; }
-
+make clean > /dev/null 2>&1 || { echo -e "\e[91mError: Unable to clean the project.\e[0m"; exit 1; }
+make tests > /dev/null 2>&1 || { echo -e "\e[91mError: Unable to build and run tests.\e[0m"; exit 1; }
+echo -e "\e[92mCompilation successful.\e[0m"
 cd tests
+all_tests=$(ls -d */ | cut -f1 -d'/')
 
-test_name="1_basic_main"
-output_file="$test_name/out"
-expected_output_file="$test_name/out_expected"
+# Initialize counters
+succeeded_tests=0
+failed_tests=0
 
-(../bin/cmat_test_version < "$test_name/in") > "$output_file" 2>&1 || { echo "Error: Unable to run test $test_name."; exit 1; }
+for test_folder in $all_tests; do
+  if [ -d "$test_folder" ]; then
+    test_name=$(basename "$test_folder")
+    output_file="$test_folder/out"
+    expected_output_file="$test_folder/out_expected"
 
-# Compare output with expected
-if diff "$output_file" "$expected_output_file" > /dev/null 2>&1; then
-  echo "Test $test_name passed successfully."
+    (../bin/cmat_test_version < "$test_folder/in") > "$output_file" 2>&1 || { 
+      echo -e "\e[91mError: Unable to run test $test_name.\e[0m"; 
+      exit 1; 
+    }
+
+    # Compare output with expected
+    if diff "$output_file" "$expected_output_file" > /dev/null 2>&1; then
+      echo -e "\e[92mTest $test_name passed successfully.\e[0m"
+      ((succeeded_tests++))
+    else
+      echo -e "\e[91mError: Test $test_name failed. Output differs from expected.\e[0m"
+      ((failed_tests++))
+    fi
+  fi
+done
+
+# Print test results
+echo -e "\n\e[93mTest Results:\e[0m"
+echo -e "\e[92mSucceeded Tests: $succeeded_tests\e[0m"
+echo -e "\e[91mFailed Tests: $failed_tests\e[0m"
+
+# Exit with appropriate status
+if [ "$failed_tests" -eq 0 ]; then
+  exit 0
 else
-  echo "Error: Test $test_name failed. Output differs from expected."
   exit 1
 fi
